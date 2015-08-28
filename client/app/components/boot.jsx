6ed553b1a39f1ea9f2f79193f78c78c698bf7633
leeric92 <hle2806@uw.edu>
2015-08-28 15:55:35 -0700

    merging

---
+++ client/app/components/boot.jsx
@@ -0,0 +1,2 @@
+React.render(<App clientId={AUTH0_CLIENT_ID} domain={AUTH0_DOMAIN} />,
+  document.getElementById('login-page'));
---
+++ client/app/components/home.jsx
@@ -0,0 +1,14 @@
+var Home = React.createClass({
+  showLock: function() {
+    this.props.lock.show();
+  },
+
+  render: function() {
+    return (
+    <div className="login-box auth0-box before">
+      <img src="../img/mipmap-xxxhdpi/ic_launcher.png" />
+      <h1>Gyftee</h1>
+      <a onClick={this.showLock} className="btn btn-primary btn-lg btn-login btn-block">Sign In</a>
+    </div>);
+  }
+});
