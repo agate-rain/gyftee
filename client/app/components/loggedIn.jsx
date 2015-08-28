b444a52f81d8d8fc289c88a402ff3bf472c85da0
leeric92 <hle2806@uw.edu>
2015-08-28 16:12:01 -0700

    add login page

--- client/app/components/loggedIn.jsx
+++ client/app/components/loggedIn.jsx
@@ -1,7 +1,7 @@
 var LoggedIn = React.createClass({
   callApi: function() {
     $.ajax({
-      url: 'http://localhost:3001/secured/ping',
+      url: 'http://localhost:3000/secured/ping',
       method: 'GET'
     }).then(function(data, textStatus, jqXHR) {
       alert("The request to the secured enpoint was successfull");
@@ -30,10 +30,10 @@ var LoggedIn = React.createClass({
     if (this.state.profile) {
       return (
         <div className="logged-in-box auth0-box logged-in">
-          <h1 id="logo"><img src="https://cdn.auth0.com/blog/auth0_logo_final_blue_RGB.png" /></h1>
+          
           <img src={this.state.profile.picture} />
           <h2>Welcome {this.state.profile.nickname}</h2>
-          <button onClick={this.callApi} className="btn btn-lg btn-primary">Call API</button>
+
         </div>);
     } else {
       return (
