c9b046ae6381b18967cbd8417984b68f678342f1
batmanimal <thejenniekim@gmail.com>
2015-08-28 11:21:07 -0700

    Define gift controller and routes
    Add skeleton for functions that we will build later

---
+++ server/gifts/giftController.js
@@ -0,0 +1,15 @@
+var Gift = require('./giftModel.js');
+
+module.exports = {
+  // call to Amazon API to get a friend's 'liked' item on facebook
+  lookupItemByKeyword: function(req, res) {
+
+  },
+  // call to Amazon API to get similar items based on the 'liked' item
+  getSimilarItems: function(req, res) {
+
+  }
+  // TODO: search etsy using image tags or other facebook metadata 
+  // and get surprise gifts (grab bag feature)
+
+};
