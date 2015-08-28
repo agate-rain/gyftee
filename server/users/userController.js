4ef95a7559ec4d2a01bf44e2478e36b0bbf836ae
batmanimal <thejenniekim@gmail.com>
2015-08-28 10:51:39 -0700

    Create user routes and controller

---
+++ server/users/userController.js
@@ -0,0 +1,13 @@
+var User = require('./userModel.js');
+
+module.exports = {
+  // TODO get username and pw from FB API
+
+  // signin 
+  // look up user in database
+    // if user does not exist, create and save in db 
+    // else, let user sign in 
+
+  // signout 
+  // remove session 
+};
