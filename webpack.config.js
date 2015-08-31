23335669e8937c19ba9496f18e74567646bf49ee
Chris Saden <csaden@gmail.com>
2015-08-31 12:02:44 -0700

    add react hot loader and set up webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
+var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'client/app/main.js'),
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+    path.resolve(__dirname, 'client/app/main.js')
+  ],
   output: {
     path: path.resolve(__dirname, 'client/build'),
     filename: 'bundle.js'
   },
-  module {
-    loaders: []
-  }
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loaders: ['react-hot', 'jsx?harmony'], include: path.join(__dirname, 'client') }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
