68baeb3ae45de596fbbc2db6ed0a11a6a4c62b35
Chris Saden <csaden@gmail.com>
2015-08-31 12:11:02 -0700

    add webpack and hot load config

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,21 @@ var path = require('path');
 
 module.exports = {
   entry: [
-    'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
     'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
     path.resolve(__dirname, 'client/app/main.js')
   ],
   output: {
-    path: path.resolve(__dirname, 'client/build'),
+    path: path.resolve(__dirname, '/build'),
     filename: 'bundle.js'
   },
   module: {
     loaders: [
-      { test: /\.jsx?$/, loaders: ['react-hot', 'jsx?harmony'], include: path.join(__dirname, 'client') }
+      { test: /\.jsx?$/, loaders: ['react-hot', 'babel'], include: path.join(__dirname, 'client'), exclude: /(node_modules|bower_components)/ },
+      { test: /\.js$/, exclude: /(node_modules|bower_components)/, loader: 'babel-loader'},
+      { test: /\.css$/, loader: "style!css" }
     ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.NoErrorsPlugin()
   ]
 };
