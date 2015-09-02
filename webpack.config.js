37d00fabf6eaa05692e4042b95c29d01ceec453c
Chris Saden <csaden@gmail.com>
2015-09-02 11:29:23 -0700

    config webpack and add comments

--- webpack.config.js
+++ webpack.config.js
@@ -2,50 +2,79 @@
 
 var webpack = require('webpack'),
   HtmlWebpackPlugin = require('html-webpack-plugin'),
-  path = require('path'),
-  srcPath = path.join(__dirname, 'src');
+  merge = require('webpack-merge'),
+  path = require('path');
 
-module.exports = {
+var TARGET = process.env.npm_lifecycle_event;
+
+//webpack requires absolute paths
+var SRC_PATH = path.join(__dirname, 'src');
+
+var common = {
   target: 'web',
   cache: true,
   entry: {
-    module: path.join(srcPath, 'app/views/friendManager.jsx'),
+    module: path.join(SRC_PATH, 'app/views/boot'),
     common: ['react', 'react-router']
   },
+  // to enable requiring files without specifying the extension
+  // you must add a resolve.extensions parameter specifying which files
+  // webpack searches for... can use require('file') instead of require('file.jsx')
   resolve: {
-    root: srcPath,
-    extensions: ['', '.js', 'jsx'],
-    modulesDirectories: ['node_modules', 'src']
+    root: [path.join(SRC_PATH, 'app/components'), path.join(SRC_PATH, 'app/views')],
+    extensions: ['', '.js', '.jsx']
   },
   output: {
-    path: path.join(__dirname, 'tmp'),
-    publicPath: '',
-    filename: '[name].js',
-    library: ['Example', '[name]'],
+    path: path.join(__dirname, 'build'), // This is where images AND js will go
+    publicPath: '', // This is used to generate URLs to e.g. images
+    filename: '[name].js', // template name based on keys in entry
     pathInfo: true
   },
 
   module: {
-    loaders: [
-      {test: /\.jsx$/, exclude: /node_modules/, loader: 'jsx-loader'},
-      {test: /\.css$/, loader: "style!css" },
-      {test: /\.png$/, loader: "url-loader?limit=100000" },
-      {test: /\.jpg$/, loader: "file-loader" }
+    loaders: [ // webpack's equivalent of browserify transforms and RequireJS plugins is a loader
+      {
+        test: /\.jsx$/,
+        loader: 'jsx-loader',
+        include: path.join(SRC_PATH, 'app')
+      },
+      {
+        test: /\.css$/,
+        loaders: 'style-loader!css-loader', // use ! to chain loaders
+        include: path.join(SRC_PATH, 'css')
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: "url-loader?limit=8192" // inline base64 URLs for <=8k images, direct URLs for the rest
+      },
+      {
+        test: /\.jpg$/,
+        loader: "file-loader"
+      }
     ]
-  },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin('common', 'common.js'),
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: 'src/index.html'
-    }),
-    new webpack.NoErrorsPlugin()
-  ],
-
-  debug: true,
-  devtool: 'eval-cheap-module-source-map',
-  devServer: {
-    contentBase: './tmp',
-    historyApiFallback: true
   }
 };
+
+if (TARGET === 'start' || !TARGET) {
+  // webpack dev server automatically refreshes content inthe browser
+  module.exports = merge(common, {
+    devtool: 'eval-cheap-module-source-map',
+    devServer: {
+      colors: true,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      port: 3000
+    },
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin('common.js'),
+      new HtmlWebpackPlugin({
+        inject: true, // injects script tag for js at end of body
+        template: 'src/index.html'
+      }),
+      new webpack.NoErrorsPlugin(),
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
