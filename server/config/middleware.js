3ddfa8ca0f1b00f62acd73daa71a3e1ad29bb254
Chris Saden <csaden@gmail.com>
2015-09-02 11:30:09 -0700

    clean up server

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -10,8 +10,7 @@ module.exports = function(app, express) {
   // define routers
   var userRouter = express.Router();
   var giftRouter = express.Router();
-  // giftList is a collection of gift items
-  var giftListRouter = express.Router();
+  var giftListRouter = express.Router(); // giftList is a collection of gift items
   var friendRouter = express.Router();
 
   // Request body parsing middleware should be above methodOverride
@@ -25,13 +24,13 @@ module.exports = function(app, express) {
 
   app.use(express.static(__dirname + '/../../client'));
 
-  // define API paths 
+  // define API paths
   app.use('/api/users', userRouter);
   app.use('/api/gifts', giftRouter);
   app.use('/api/giftlists', giftListRouter);
   app.use('/api/friends', friendRouter)
 
-  // auth middleware will be here if we allow users to login w/o facebook 
+  // auth middleware will be here if we allow users to login w/o facebook
 
   // require route files
   require('../users/userRoutes.js')(userRouter);
--- server/server.js
+++ server/server.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var cookieSession = require('cookie-session');
 var db = require('./config/dbConfig');
 var jwt = require('express-jwt');
 var dotenv = require('dotenv');
@@ -15,11 +12,6 @@ var cors = require('cors');
 //load .env file
 dotenv.load();
 
-// var authenticate = jwt({
-//   secret: new Buffer(process.env.AUTH0_CLIENT_SECRET, 'base64'),
-//   audience: process.env.AUTH0_CLIENT_ID
-// });
-
 var app = express();
 
 // configure the server with all the middleware and the routing
@@ -27,62 +19,53 @@ require('./config/middleware')(app, express);
 
 var prodAdv = aws.createProdAdvClient(process.env.AMAZON_CLIENT_ID, process.env.AMAZON_CLIENT_SECRET, process.env.AMAZON_ASSOCIATE_TAG);
 
-// Request body parsing middleware should be above methodOverride
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(cookieSession({secret: process.env.SESSION_SECRET}));
-
-// app.use('/secured', authenticate);
-app.use(cors());
-
 app.get('/ping', function(req, res) {
   res.send(200, {text: "All good. You don't need to be authenticated to call this"});
 });
 
-app.post('/api/friends', function(req, res) {
-    BPromise.all([
-        facebookApi.friends(req.body.access_token),
-        facebookApi.invitableFriends(req.body.access_token)
-    ])
-    .spread(function(friendsResponse, invitableFriendsResponse) {
-        var friends = friendsResponse.data.map(function(userData) {
-            // console.log(JSON.stringify(userData,null, '\t'));
-            return {
-                id: userData.id,
-                name: userData.name,
-                pictureUrl: userData.picture.data.url,
-                birthday : userData.birthday,
-                hometown : userData.hometown.name,
-                fav_atheletes : userData.favorite_athletes,
-                inspirational_people : userData.inspirational_people,
-                sports : userData.sports,
-                books: userData.books,
-                albums: userData.albums
-            };
-        });
-        var invitableFriends = invitableFriendsResponse.data.map(function(userData) {
-            return {
-                id: userData.id,
-                name: userData.name,
-                pictureUrl: userData.picture.data.url,
-                birthday : userData.birthday,
-                fav_atheletes : userData.favorite_athletes,
-                inspirational_people : userData.inspirational_people,
-                sports : userData.sports,
-                books: userData.books,
-                albums: userData.albums
-            };
-        });
-        var allFriends = friends.concat(invitableFriends);
-        console.log(JSON.stringify(invitableFriends, null, '\t'));
-        res.send(JSON.stringify(allFriends));
-    })
-    .catch(function(err) {
-        log.error('Error building friends response: ' + util.inspect(err));
-        next(err);
-    });
-});
+// app.post('/api/friends', function(req, res) {
+//     BPromise.all([
+//         facebookApi.friends(req.body.access_token),
+//         facebookApi.invitableFriends(req.body.access_token)
+//     ])
+//     .spread(function(friendsResponse, invitableFriendsResponse) {
+//         var friends = friendsResponse.data.map(function(userData) {
+//             // console.log(JSON.stringify(userData,null, '\t'));
+//             return {
+//                 id: userData.id,
+//                 name: userData.name,
+//                 pictureUrl: userData.picture.data.url,
+//                 birthday : userData.birthday,
+//                 hometown : userData.hometown.name,
+//                 fav_atheletes : userData.favorite_athletes,
+//                 inspirational_people : userData.inspirational_people,
+//                 sports : userData.sports,
+//                 books: userData.books,
+//                 albums: userData.albums
+//             };
+//         });
+//         var invitableFriends = invitableFriendsResponse.data.map(function(userData) {
+//             return {
+//                 id: userData.id,
+//                 name: userData.name,
+//                 pictureUrl: userData.picture.data.url,
+//                 birthday : userData.birthday,
+//                 fav_atheletes : userData.favorite_athletes,
+//                 inspirational_people : userData.inspirational_people,
+//                 sports : userData.sports,
+//                 books: userData.books,
+//                 albums: userData.albums
+//             };
+//         });
+//         var allFriends = friends.concat(invitableFriends);
+//         console.log(JSON.stringify(invitableFriends, null, '\t'));
+//         res.send(JSON.stringify(allFriends));
+//     })
+//     .catch(function(err) {
+//         log.error('Error building friends response: ' + util.inspect(err));
+//         next(err);
+//     });
+// });
 
 mongoose.connect(db.url);
 mongoose.connection.once('connected', function(){
