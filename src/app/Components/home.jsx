b3611b741aa8b0de31a32f16a0236267a8260802
batmanimal <thejenniekim@gmail.com>
2015-09-05 19:52:49 -0700

    Create pretty login view

--- src/app/Components/home.jsx
+++ src/app/Components/home.jsx
@@ -7,24 +7,23 @@ var Home = React.createClass({
 
   render: function() {
     return (
-      <View row expand>
-    <div className="login-container flex-container responsive">
+      
+    <div className="login-container container-fluid flex-container responsive">
 
       <div className="container">
 
-      <div className="logo">
-        <div className="icon-container"><img src="src/client/img/gyftee-icon.png"></img></div>
+      <div className="logo responsive">
+        <div className="icon-container"><img src="src/client/img/g-icon.png"></img></div>
         <div className="brand-container"><span className="gyftee-text">gyftee</span></div>
       </div>
 
       <div className="login-button-container">
-      <a onClick={this.showLock} className="login-button btn">Sign In</a>
+      <a onClick={this.showLock} className="btn btn-default dark-teal proxima white-font btn-block btn">Sign In</a>
       </div>
 
     </div>
    
   </div>
-  </View>
   );
   }
 });
