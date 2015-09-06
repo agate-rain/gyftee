acdb600e516c8e516c00e533f9c533c1d196c142
Chris Saden <csaden@gmail.com>
2015-09-05 18:53:59 -0700

    add navigation mixin and func to navToRecs

--- src/app/Components/friendList.jsx
+++ src/app/Components/friendList.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Friend from './friend';
 import { removeFriend } from '../Actions/user';
+import { Navigation } from 'react-router';
 
 var FriendList = React.createClass({
 
+  mixins: [ Navigation ],
+
   clickHandler: function(id) {
     this.props.dispatch(removeFriend(id));
+  },
+
+  navToGiftList: function(id) {
+    this.transitionTo(`/friends/${id}`);
   },
 
   render: function() {
@@ -16,7 +23,10 @@ var FriendList = React.createClass({
           this.props.appFriends.map(function(friend) {
 
             return (
-              <Friend friend={friend} key={friend.id} onClick={this.clickHandler.bind(this, friend.id)}/>
+              <div>
+                <button onClick={this.clickHandler.bind(this, friend.id)}>Remove</button>
+                <Friend friend={friend} key={friend.id} onClick={this.navToGiftList.bind(this, friend.id)}/>
+              </div>
             );
 
           }, this)
