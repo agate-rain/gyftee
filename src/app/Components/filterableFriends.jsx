8de584c7d39fe3566c2b2351b1272937b89775d8
leeric92 <hle2806@uw.edu>
2015-09-07 11:26:19 -0700

    get filterableFriend to work with all facebook Friend

--- src/app/Components/filterableFriends.jsx
+++ src/app/Components/filterableFriends.jsx
@@ -11,17 +11,11 @@ var FilterableFriends = React.createClass({
   },
 
   filterFacebookFriends: function(e) {
-    console.log(this.state);
     this.setState({filterText: e.target.value});
-
     if (this.state.filterText.length === 0) {
-
       this.setState({resultsVisible: false});
-
     } else {
-
       this.setState({resultsVisible: true});
-
     }
   },
 
@@ -30,19 +24,15 @@ var FilterableFriends = React.createClass({
   },
 
   render: function() {
-    var friends = this.props.fbFriends,
+    var friends = this.props.appFriends === undefined ? this.props.fbFriends : this.props.appFriends,
         filterText = this.state.filterText.trim().toLowerCase();
-        // friends = JSON.parse(friends);
-        // friends = JSON.parse(friends);
-        // console.log(friends)
-        // friends = JSON.parse(friends);
     if (filterText.length > 0) {
       friends = friends.filter(function(friend) {
         return friend.name.toLowerCase().match( filterText );
       });
     }
     return (
-      <div className="filtered-friends">
+      <div className="filtered-friends" id= "filtered-friends">
         <input ref="filterText" type="text" placeholder="Search for your friends" value={this.state.filterText} onChange={this.filterFacebookFriends}/>
         { this.state.resultsVisible ? <SearchResults friends={friends}/> : null }
       </div>
