436338d811646b602af5618707b0155611a030b7
leeric92 <hle2806@uw.edu>
2015-09-07 11:25:42 -0700

    add invite button and stylign to facebooFriend component

--- src/app/Components/facebookFriend.jsx
+++ src/app/Components/facebookFriend.jsx
@@ -4,7 +4,8 @@ var FacebookFriend = React.createClass({
   //displays facebook friend using app when searching for facebook friends
   render: function() {
     return (
-      <div className='friend-row'>
+      <div className='all-friend-row'>
+        <button className='invite-button'>Invite</button>
         <div className='friend-picture'>{this.props.friend.picture}</div>
         <div className='friend-name'>{this.props.friend.name}</div>
       </div>
