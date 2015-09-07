c0ae5def3ac19a60b01f01b47448a5723a530079
leeric92 <hle2806@uw.edu>
2015-09-07 11:29:22 -0700

    no changes made

--- src/app/Components/searchResults.jsx
+++ src/app/Components/searchResults.jsx
@@ -5,11 +5,16 @@ var FacebookFriend = require('./facebookFriend');
 var SearchResults = React.createClass({
   render: function() {
     var facebookFriends = [];
+
     this.props.friends.forEach(function(friend) {
       facebookFriends.push(<FacebookFriend friend={friend} key={friend.id}/>);
     });
     return (
-      <div className='search-results'>{facebookFriends}</div>
+        <div className="bday-list-container">
+          <div className="bday-list-body seafoam">
+            {facebookFriends}
+          </div>
+        </div>
     )
   }
 });
