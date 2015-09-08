7b95571eb23744e1d866f5b27d48eb594ba78581
batmanimal <thejenniekim@gmail.com>
2015-09-08 10:18:40 -0700

    Add dependencies to pinned list container

--- src/app/Containers/pinnedGiftList.jsx
+++ src/app/Containers/pinnedGiftList.jsx
@@ -1,4 +1,22 @@
 import React from 'react';
-import Book from '../Components/book';
 import { connect } from 'react-redux';
-import { BOOK } from '../../../data/hardCoded';
+import { BOOK } from '../Components/book';
+
+var PinnedGiftList = React.createClass({
+
+  render: function() {
+    return (
+      <div className="gift">
+      <Book book={this.props.gift}/>
+      </div>
+    );
+  }
+});
+
+var mapStateToProps = function(state) {
+  return {
+    gift : state.gift // export the portion of the state from index.js Reducers
+  }
+};
+
+export default connect(mapStateToProps)(PinnedGiftList);
