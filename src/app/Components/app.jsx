d0529e7d3012e00ea15906402d7d3d44a4a1e678
Chris Saden <csaden@gmail.com>
2015-09-09 17:32:05 -0700

    add options for auth0 login, TODO can make additional styling changes to the lock

--- src/app/Components/app.jsx
+++ src/app/Components/app.jsx
@@ -4,30 +4,39 @@ var Home = require('./home');
 
 
 var App = React.createClass({
+
   componentWillMount: function() {
     this.setupAjax();
     this.createLock();
     this.setState({idToken: this.getIdToken()})
   },
+
   createLock: function() {
     this.lock = new Auth0Lock(this.props.clientId, this.props.domain);
+    this.lock.show({
+      connections: ['facebook'],
+      icon: '../../src/client/img/gyftee-icon.png',
+      popup: true
+    });
   },
+
   setupAjax: function() {
     $.ajaxSetup({
       'beforeSend': function(xhr) {
         if (localStorage.getItem('userToken')) {
           xhr.setRequestHeader('Authorization',
-                'Bearer ' + localStorage.getItem('userToken'));
+            'Bearer ' + localStorage.getItem('userToken'));
         }
       }
     });
   },
+
   getIdToken: function() {
     var idToken = localStorage.getItem('userToken');
     var authHash = this.lock.parseHash(window.location.hash);
     if (!idToken && authHash) {
       if (authHash.id_token) {
-        idToken = authHash.id_token
+        idToken = authHash.id_token;
         localStorage.setItem('userToken', authHash.id_token);
       }
       if (authHash.error) {
@@ -36,6 +45,7 @@ var App = React.createClass({
     }
     return idToken;
   },
+
   render: function() {
     if (this.state.idToken) {
       return (<LoggedIn lock={this.lock} idToken={this.state.idToken} />);
