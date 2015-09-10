9d3d1115041aba606310b05e8b454c6815e69b28
Ben Biran <benbiran@gmail.com>
2015-09-09 21:32:16 -0700

    add titles  to carousel

--- src/app/Components/book.jsx
+++ src/app/Components/book.jsx
@@ -82,6 +82,16 @@ var Book = React.createClass({
               <div className="book-binding">{bookDetails.binding}</div>
             </div>
 
+            {(() => {
+              if (bookDetails.isPrime){
+                return (
+                  <div className="prime-eligible">
+                    <img className ="prime-img" src="../../src/client/img/amazon-prime.png" />
+                  </div>
+                  );
+              }
+            })()}
+
             <div className="based-on-container light-teal">
               <p className="based-on-text">Based on: {basedOnDetails.title}</p>
             </div>
@@ -91,15 +101,7 @@ var Book = React.createClass({
     );
   }
 });
-          // {(() => {
-          //   if (bookDetails.isPrime){
-          //     return (
-          //       <div className="prime-eligible">
-          //         <img className ="prime-img" src="https://b1.burst.zone/wp-content/uploads/2014/03/amazon-prime-logo.jpg" />
-          //       </div>
-          //       );
-          //   }
-          // })()}
+
 
 var mapStateToProps = function(state) {
   return {
