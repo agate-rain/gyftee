7bfac0bf0ca8ada7b5a28406f3ed8bdb73f4520a
Chris Saden <csaden@gmail.com>
2015-09-11 12:50:22 -0700

    resolve home conflicts

--- src/app/Components/home.jsx
+++ src/app/Components/home.jsx
@@ -2,22 +2,25 @@ import React from 'react';
 
 var Home = React.createClass({
   showLock: function() {
-    this.props.lock.show();
+    this.props.lock.show({
+      connections: ['facebook'],
+      icon: '../../src/client/img/gyftee-icon.png'
+    });
   },
 
   render: function() {
     return (
-      <div className="flex-container">
-        <div className="login-container">
+      <div className="container login-container">
+        <div className="container">
           <div className="logo responsive">
             <div className="icon-container"><img src="src/client/img/g-icon.png"></img></div>
-            <div className="brand-container"><span className="gyftee-text">gyftee</span></div>
+            <div className="brand-container"><span className="gyftee-text">Gyftee</span></div>
           </div>
           <button className="login-button" onClick={this.showLock}>
-            <a className="white-font opacity">LOG IN</a>
+            <a className="white-font opacity">SIGN IN</a>
           </button>
+        </div>
       </div>
-    </div>
     );
   }
 });
