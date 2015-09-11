00873e7f2e2d928fade814d0cfa5cac271ef94b9
Ben Biran <benbiran@gmail.com>
2015-09-10 18:51:46 -0700

    toggle categories slider

--- src/app/Components/bookList.jsx
+++ src/app/Components/bookList.jsx
@@ -11,6 +11,27 @@ var BookList = React.createClass({
 
   mixins: [ Navigation ],
 
+  handleClick: function(){
+    if(this.state.open) {
+      this.setState({
+        open: false,
+        class: "section"
+      });
+    }else{
+      this.setState({
+        open: true,
+        class: "section open"
+      });
+    }
+  },
+
+  getInitialState: function(){
+     return {
+       open: false,
+       class: "section"
+     }
+  },
+
   navToGiftDetail: function(id) {
     this.transitionTo(`/gifts/${id}`);
   },
@@ -26,15 +47,17 @@ var BookList = React.createClass({
     }
 
     return (
-      <div className="books-list">
-        <div className="row light-teal category">
-          <div className="category-header">Books</div>
-        </div>
-          <div className="slider-container">
-            <Slider {...sliderSettings} className="books-list">
-              {books}
-            </Slider>
+      <div className={this.state.class}>
+        <div className="books-list">
+          <div className="row light-teal category">
+            <div className="category-header sectionhead" onClick={this.handleClick} >Books</div>
           </div>
+            <div className="slider-container articlewrap">
+              <Slider {...sliderSettings} className="books-list article">
+                {books}
+              </Slider>
+            </div>
+        </div>
       </div>
       );
   }
