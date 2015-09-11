d636e122fa8eebd161949cecda185454ffe0e6c2
Chris Saden <csaden@gmail.com>
2015-09-11 12:45:42 -0700

    home login page responsive

--- src/app/Components/home.jsx
+++ src/app/Components/home.jsx
@@ -10,18 +10,16 @@ var Home = React.createClass({
 
   render: function() {
     return (
-    <div className="container login-container">
-      <div className="container">
-        <div className="logo responsive">
+      <div className="login-container">
+        <div className="logo-responsive">
           <div className="icon-container"><img src="src/client/img/g-icon.png"></img></div>
           <div className="brand-container"><span className="gyftee-text">Gyftee</span></div>
         </div>
-        <a className="white-font login opacity" onClick={this.showLock}>
-          <button className="login-button">SIGN IN</button>
-        </a>
-    </div>
-  </div>
-  );
+        <button className="login-button" onClick={this.showLock}>
+          <a className="white-font opacity">LOG IN</a>
+        </button>
+      </div>
+    );
   }
 });
 
