d7fff98795afa5763f3dcafee9e4b3ad88a135e2
Chris Saden <csaden@gmail.com>
2015-09-12 16:50:39 -0700

    add message and link when no pinned gifts

--- src/app/Components/wishlist.jsx
+++ src/app/Components/wishlist.jsx
@@ -10,63 +10,73 @@ var WishList = React.createClass({
 
   componentDidMount: function() {
     var friendId = this.props.friend.friend.id;
-    utils.fetchFriendById(friendId, function(friend){
+    utils.fetchFriendById(friendId, function(friend) {
       this.props.dispatch(fetchFriend(friend));
     }.bind(this));
 
-    utils.fetchImageUrlById(friendId, function(image){
+    utils.fetchImageUrlById(friendId, function(image) {
       this.props.dispatch(saveImageUrl(image));
     }.bind(this));
   },
 
   render: function() {
-    console.log('props', this.props);
     var friend = this.props.friend.friend;
     var url = this.props.friend.image_url;
-
+    var books = this.props.wishlist.books;
+    // TODO REFACTOR TO SHOW ALL GIFTS, NOT JUST BOOKS
     var wishListItems = [];
-    for(var i = 0; i < this.props.wishlist.length; i++){
+    for (var i = 0; i < books.length; i++) {
       wishListItems.push(
-          <WishListBook user = {this.props.user} friend={this.props.friend.friend} book={this.props.wishlist[i]} key={this.props.wishlist[i].ASIN} removeItem={this.props.removeItem} />
+        <WishListBook user={this.props.user} friend={this.props.friend.friend}
+          book={books[i]} key={books[i].ASIN}
+          removeItem={this.props.removeItem} />
       );
     }
-    if(friend !== null){
 
-      return (
-        <div>
-          <div className="flex-container">
-            <div className="giftlist-main container">
-
-              <div className="container flex-container">
-                <div className="wishlist-header">
-                  <div className="friend-header-name">{friend.name}</div>
-                  <div className="friend-header-bday">{formatDate(friend.birthday)}</div>
-
-                </div>
-
-                <div className="wishlist-header">
-                  <div className="wishlist-header-aside">
-                    <div className="thumbnail profile-photo friend-thumbnail">
-                      <img src={url} />
-                    </div>
-                    <div className="container giftlist-circle"><text>{this.props.wishlist.length}</text></div>
+    return (
+      <div>
+        <div className="flex-container">
+          <div className="giftlist-main container">
+            <div className="container flex-container">
+              <div className="wishlist-header">
+                <div className="friend-header-name">{friend.name}</div>
+                <div className="friend-header-bday">{formatDate(friend.birthday)}</div>
+              </div>
+              <div className="wishlist-header">
+                <div className="wishlist-header-aside">
+                  <div className="thumbnail profile-photo friend-thumbnail">
+                    <img src={url} />
+                  </div>
+                  <div className="container giftlist-circle">
+                    <text>{this.props.items}</text>
                   </div>
                 </div>
+              </div>
+            </div>{/* flex-container of friend info */}
+          </div>{/* giftlist-main container */}
+        </div>{/* flex-container */}
 
-              </div>{/* flex-container of friend info */}
+        <div className="giftlist-header row light-teal">Saved Gifts</div>
 
-            </div>{/* giftlist-main container */}
-          </div>{/* flex-container */}
+        {(() => {
 
-          <div className="giftlist-header row light-teal">Saved Gifts</div>
-
-          <div className="flex-container pinned-gifts">
-            {wishListItems}
-          </div>
-        </div>
-
-      );
-    }
+          if (wishListItems.length > 0) {
+            return (
+              <div className="flex-container pinned-gifts">{wishListItems}</div>
+            );
+          } else {
+            return (
+              <div>
+                <div>No pinned gifts</div>
+                <div className="button recommendations-button" onClick={this.props.navToFriendRecs.bind(this, this.props.friend.friend.id)}>
+                  SEE GIFT RECOMMENDATIONS
+                </div>
+              </div>
+            );
+          }
+        })()}
+      </div>
+    );
   }
 });
 
@@ -75,6 +85,5 @@ var mapStateToProps = function(state) {
     state
   }
 };
-
 
 export default connect(mapStateToProps)(WishList);
