d80a62a9625492b8c94101b97e4855db82db949d
batmanimal <thejenniekim@gmail.com>
2015-09-15 15:00:21 -0700

    deprecate unneeded files

--- src/app/Components/bookList.jsx
+++ src/app/Components/bookList.jsx
@@ -1,57 +1,57 @@
-import React from 'react';
-import Thumbnail from './thumbnail';
-import Book from './book';
-import { Navigation, Link } from 'react-router';
-import sliderSettings from '../../util/sliderSettings';
-import Slider from 'react-slick';
+// import React from 'react';
+// import Thumbnail from './thumbnail';
+// import GiftItem from './giftItem';
+// import { Navigation, Link } from 'react-router';
+// import sliderSettings from '../../util/sliderSettings';
+// import Slider from 'react-slick';
 
-var BookList = React.createClass({
+// var BookList = React.createClass({
 
-  mixins: [ Navigation ],
+//   mixins: [ Navigation ],
 
-  handleClick: function(){
-    if (this.state.open) {
-      this.setState({open: false, class: "section"});
-    } else {
-      this.setState({open: true, class: "section open"});
-    }
-  },
+//   handleClick: function(){
+//     if (this.state.open) {
+//       this.setState({open: false, class: "section"});
+//     } else {
+//       this.setState({open: true, class: "section open"});
+//     }
+//   },
 
-  getInitialState: function(){
-     return {open: true, class: "section open"}
-  },
+//   getInitialState: function(){
+//      return {open: true, class: "section open"}
+//   },
 
-  // navToGiftDetail: function(id) {
-  //   this.transitionTo(`/gifts/${id}`);
-  // },
+//   // navToGiftDetail: function(id) {
+//   //   this.transitionTo(`/gifts/${id}`);
+//   // },
 
-  render: function() {
-    var books = [];
-    for(var key in this.props.amazonBooks){
-      books.push(
-        <div>
-          <Thumbnail book={this.props.amazonBooks[key]} key={this.props.amazonBooks[key].ASIN} />
-        </div>
-      );
-    }
+//   render: function() {
+//     var books = [];
+//     for(var key in this.props.amazonBooks){
+//       books.push(
+//         <div>
+//           <Thumbnail book={this.props.amazonBooks[key]} key={this.props.amazonBooks[key].ASIN} />
+//         </div>
+//       );
+//     }
 
-    return (
-      <div className={this.state.class}>
-        <div className="books-list">
+//     return (
+//       <div className={this.state.class}>
+//         <div className="books-list">
 
-          <div className="row light-teal category sectionhead" onClick={this.handleClick}>
-            <button>toggle</button>
-            <div className="category-header">Books</div>
-          </div>
-            <div className="slider-container sliderwrap">
-              <Slider {...sliderSettings} className="books-list slider">
-                {books}
-              </Slider>
-            </div>
-        </div>
-      </div>
-      );
-  }
-});
+//           <div className="row light-teal category sectionhead" onClick={this.handleClick}>
+//             <button>toggle</button>
+//             <div className="category-header">Books</div>
+//           </div>
+//             <div className="slider-container sliderwrap">
+//               <Slider {...sliderSettings} className="books-list slider">
+//                 {books}
+//               </Slider>
+//             </div>
+//         </div>
+//       </div>
+//       );
+//   }
+// });
 
-export default BookList;
+// export default BookList;
