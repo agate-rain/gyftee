298b57e20b8edf7a9c6c1663ce2e677900f46653
batmanimal <thejenniekim@gmail.com>
2015-09-15 14:09:56 -0700

    deprecate unneeded components and container

--- src/app/Containers/concertDetail.jsx
+++ src/app/Containers/concertDetail.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import Concert from '../Components/concert';
-import NavBar from '../Components/navbar';
-import { connect } from 'react-redux';
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import GiftItem from '../Components/giftItem';
+// import NavBar from '../Components/navbar';
 
-var ConcertDetail = React.createClass({
+// var ConcertDetail = React.createClass({
 
-  render: function() {
-    return (
-      <div>
-      <NavBar />
-      <Concert concert={this.props.gift}/>
-      </div>
-    );
-  }
-});
+//   render: function() {
+//     return (
+//       <div>
+//       <NavBar />
+//       <GiftItem concert={this.props.gift}/>
+//       </div>
+//     );
+//   }
+// });
 
-var mapStateToProps = function(state) {
-  return {
-    gift : state.gift // export the portion of the state from index.js Reducers
-  }
-};
+// var mapStateToProps = function(state) {
+//   return {
+//     gift : state.gift // export the portion of the state from index.js Reducers
+//   }
+// };
 
-export default connect(mapStateToProps)(ConcertDetail);
+// export default connect(mapStateToProps)(ConcertDetail);
