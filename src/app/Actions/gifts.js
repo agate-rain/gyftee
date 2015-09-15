5503a9526edca479b10daf23470bf1b632644dbb
Chris Saden <csaden@gmail.com>
2015-09-14 20:15:08 -0700

    code clean up

--- src/app/Actions/gifts.js
+++ src/app/Actions/gifts.js
@@ -2,14 +2,14 @@ import * as types from '../Constants/ActionTypes';
 
 export function saveGifts(arrayGift) {
   return {
-    type : types.SAVE_GIFTS,
-    gifts : arrayGift
+    type: types.SAVE_GIFTS,
+    gifts: arrayGift
   };
 };
 
 export function initGifts() {
   return {
-    type : types.INIT_GIFTS
+    type: types.INIT_GIFTS
   };
 };
 
